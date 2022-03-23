704d71e9219d5e4a20285234b425381deb49e5ee
Simabharti <101928620+Simabharti@users.noreply.github.com>
2022-03-24 00:38:44 +0530

    Add files via upload

---
+++ duplicat.java
@@ -0,0 +1,24 @@
+package com.sima;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class duplicat {
+    public static void main(String[] args) {
+        int  [] array1 = { 12,34,85,12,43,24 ,34,24};
+        System.out.println("print only duplicate value in array:--");
+        for (int i = 0; i < array1.length; i++) {
+            for (int j = i + 1; j < array1.length; j++) {
+
+            }
+            }
+            for (int i = 0; i < array1.length; i++) {
+                for (int j = i + 1; j < array1.length; j++) {
+                    if (array1[i] == array1[j])
+                        if (array1[i] == array1[j])
+
+                            System.out.println(array1 [j]);
+                }
+            }
+        }}
+
